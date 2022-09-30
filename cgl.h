5625d7000686433510aebb311f8e6e7a4649d9ab
Jaysmito Mukherjee <73700725+Jaysmito101@users.noreply.github.com>
2022-09-30 12:31:17 +0530

    Added Super Fast Hash
    Refer: http://www.azillionmonkeys.com/qed/hash.html

--- cgl.h
+++ cgl.h
@@ -121,6 +121,7 @@ void CGL_list_fill(CGL_list* list, size_t size);
 uint32_t CGL_utils_crc32(const void* data, size_t size);
 uint64_t CGL_utils_crc64(const void* data, size_t size);
 void CGL_utils_rot13(const char* data_in, char* data_out);
+uint32_t CGL_utils_super_fast_hash(const void* data, size_t size);
 
 // threads
 #ifndef CGL_EXCLUDES_THREADS
@@ -1365,6 +1366,67 @@ void CGL_utils_rot13(const char* data_in, char* str)
       if (str[idx] < 26)
          str[idx] += case_type;
    }
+}
+
+
+// From : http://www.azillionmonkeys.com/qed/hash.html
+
+#if (defined(__GNUC__) && defined(__i386__)) || defined(__WATCOMC__) \
+  || defined(_MSC_VER) || defined (__BORLANDC__) || defined (__TURBOC__)
+#define CGL_get16bits(d) (*((const uint16_t *) (d)))
+#endif
+
+#if !defined (CGL_get16bits)
+#define CGL_get16bits(d) ((((uint32_t)(((const uint8_t *)(d))[1])) << 8)\
+                       +(uint32_t)(((const uint8_t *)(d))[0]) )
+#endif
+
+uint32_t CGL_utils_super_fast_hash(const void* dat, size_t len)
+{
+    const uint8_t* data = (const uint8_t*)dat;
+    uint32_t hash = (uint32_t)len;
+    uint32_t tmp;
+    int rem;
+    
+    if (len <= 0 || data == NULL) return 0;
+
+    rem = len & 3;
+    len >>= 2;
+
+    /* Main loop */
+    for (;len > 0; len--) {
+        hash  += CGL_get16bits (data);
+        tmp    = (CGL_get16bits (data+2) << 11) ^ hash;
+        hash   = (hash << 16) ^ tmp;
+        data  += 2*sizeof (uint16_t);
+        hash  += hash >> 11;
+    }
+
+    /* Handle end cases */
+    switch (rem) {
+        case 3: hash += CGL_get16bits (data);
+                hash ^= hash << 16;
+                hash ^= ((signed char)data[sizeof (uint16_t)]) << 18;
+                hash += hash >> 11;
+                break;
+        case 2: hash += CGL_get16bits (data);
+                hash ^= hash << 11;
+                hash += hash >> 17;
+                break;
+        case 1: hash += (signed char)*data;
+                hash ^= hash << 10;
+                hash += hash >> 1;
+    }
+
+    /* Force "avalanching" of final 127 bits */
+    hash ^= hash << 3;
+    hash += hash >> 5;
+    hash ^= hash << 4;
+    hash += hash >> 17;
+    hash ^= hash << 25;
+    hash += hash >> 6;
+
+    return hash;
 }
 
 #endif
