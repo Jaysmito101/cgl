b061e1d0b994e859b382f0d9f1f01ef116ca0f6c
Jaysmito Mukherjee <73700725+Jaysmito101@users.noreply.github.com>
2022-10-01 12:08:46 +0530

    Create using_hashtable.c

---
+++ examples/using_hashtable.c
@@ -0,0 +1,29 @@
+#define CGL_LOGGING_ENABLED
+#define CGL_IMPLEMENTATION
+#include "cgl.h"
+
+
+int main()
+{
+    char buffer[1024];
+
+    CGL_hashtable* table = CGL_hashtable_create(10000, 0);
+    int idata = 0;
+    float fdata = 0.0f;
+    CGL_hashtable_set_string(table, "Name", "Jaysmito");
+    CGL_hashtable_set_int(table, "Age", 18);
+    CGL_hashtable_set_float(table, "Float Data", 54584.4584f);
+
+    // print 
+    printf("Age : %d\n", CGL_hashtable_get_int(table, "Age"));
+    printf("Float Data : %f\n", CGL_hashtable_get_float(table, "Float Data"));
+    CGL_hashtable_get(table, "Name", buffer);
+    printf("Name : %s\n", buffer);
+    CGL_hashtable_set_string(table, "Name", "Jack");
+    CGL_hashtable_get(table, "Name", buffer);
+    printf("Name 2 : %s\n", buffer);
+    CGL_hashtable_destroy(table);
+
+ 
+    return 0;
+}
