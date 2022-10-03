b032de6dffa1d48301dcd3bb25c11aac6200d422
Jaysmito Mukherjee <73700725+Jaysmito101@users.noreply.github.com>
2022-10-03 21:21:40 +0530

    Update simple_tcp_client.c

--- examples/simple_tcp_client.c
+++ examples/simple_tcp_client.c
@@ -2,14 +2,6 @@
 #define CGL_IMPLEMENTATION
 #include "cgl.h"
 
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
-
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
-
-
-
 int main()
 {
     if(!CGL_net_init()) return EXIT_FAILURE;
