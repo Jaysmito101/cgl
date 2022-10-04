842d3ae10919f731cf9f2679d48c0d80370a435d
Jaysmito Mukherjee <73700725+Jaysmito101@users.noreply.github.com>
2022-10-04 13:21:55 +0530

    Update simple_ssl_tcp_client.c

--- examples/simple_ssl_tcp_client.c
+++ examples/simple_ssl_tcp_client.c
@@ -2,12 +2,6 @@
 #define CGL_IMPLEMENTATION
 #include "cgl.h"
 
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
-
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
-
 
 int main()
 {
