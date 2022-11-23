b2ea785dce29c56c82bc308ac589501c771e86b2
shahank <crazykavlogs@gmail.com>
2022-11-23 18:35:15 +0530

    Added and modified components - Designed a navbar 	- Added links and a searchbar (React router will be necessary!) 	- Made the navbar responsive and added aria-controls - Modified the content 	- Included the CGL logo 	- Changed a bit of the introductory text

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,31 @@
+import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
+import { Form, Button, Nav } from 'react-bootstrap';
 
 export default function Header() {
   return ( 
-    <Container>
-      <h1>This is a title</h1>
-      <p>This is a header.</p>
-      <span>There should probably be a menu here too, but idk.</span>
-    </Container>
+    <Navbar bg="dark" variant="dark" sticky="top" expand="lg">
+      <Container fluid>
+        <Navbar.Brand href="#" className="site-heading">CGL</Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        
+        <Navbar.Collapse id="navBarScroll">
+          <Nav className="me-auto my-2 my-lg-0">
+            <Nav.Link href="#" style={{ color: 'white' }}>Docs</Nav.Link>
+            <Nav.Link href="#" style={{ color: 'white' }}>Examples</Nav.Link>
+          </Nav>
+
+          <Form className="d-flex">
+            <Form.Control 
+              type="search"
+              placeholder="# TODO: search"
+              className="header-searchbar me-2"
+            />
+            <Button variant="success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
