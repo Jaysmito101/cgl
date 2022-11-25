609aa66aadc229ef87d189910027c1a88815bd10
shahank <crazykavlogs@gmail.com>
2022-11-25 22:11:02 +0530

    Big changes - changed the entire stack to use react.js and tailwindcss deployed using vite - added a navbar (which sets the color theme of the website)

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+// Custom component imports
+import Header from './components/Header';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import Container from 'react-bootstrap/Container';
-
-export default function Header() {
-  return ( 
-    <Container className="footer">
-      <p>This is a footer.</p>
-    </Container>
-  );
-}
