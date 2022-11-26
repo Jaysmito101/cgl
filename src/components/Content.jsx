9c7ff3197e1816d2a00842acd1735864a6af229a
shahank <crazykavlogs@gmail.com>
2022-11-26 22:49:08 +0530

    Created a few more main components and set the stage for adding the page banner.

---
+++ src/components/Content.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Banner from './Banner';
+
+export default function Content() {
+  return (
+    <>
+      <Banner />
+      <p>Content goes here.</p>
+    </>
+  );
+}
