6a7b33d71308911b79d3acf7c987a0a1f077a98d
Jaysmito Mukherjee <73700725+Jaysmito101@users.noreply.github.com>
2022-12-10 21:06:20 +0530

    Update pong_game.c

--- examples/pong_game.c
+++ examples/pong_game.c
@@ -1,3 +1,18 @@
+// Copyright Jaysmito Mukherjee, 2023
+// I am the sole copyright owner of this Work.
+// You cannot host, display, distribute or share this Work neither
+// as it is or altered, here on Shadertoy or anywhere else, in any
+// form including physical and digital. You cannot use this Work in any
+// commercial or non-commercial product, website or project. You cannot
+// sell this Work and you cannot mint an NFTs of it or train a neural
+// network with it without permission. I share this Work for educational
+// purposes, and you can link to it, through an URL, proper attribution
+// and unmodified screenshot, as part of your educational material. If
+// these conditions are too restrictive please contact me and we'll
+// definitely work it out.
+// email : jaysmito101@gmail.com
+
+
 #define CGL_LOGGING_ENABLED
 #define CGL_EXCLUDE_NETWORKING
 #define CGL_IMPLEMENTATION
