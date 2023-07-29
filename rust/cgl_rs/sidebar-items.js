e4cb2c09f80728bbd7171f7854267f805271640e
jaysmito <jaysmito101@gmail.com>
2023-07-29 22:51:57 +0530

    added new rust wip doc

---
+++ rust/cgl_rs/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["Action","Key","MouseButton"],"fn":["init","log","shutdown"],"struct":["Window"]};
---
+++ rust/source-files.js
@@ -0,0 +1,6 @@
+var sourcesIndex = JSON.parse('{\
+"cgl_rs":["",[],["lib.rs","window_manager.rs"]],\
+"libc":["",[["windows",[["msvc",[],["mod.rs"]]],["mod.rs"]]],["fixed_width_ints.rs","lib.rs","macros.rs"]],\
+"sandbox":["",[],["main.rs"]]\
+}');
+createSourceSidebar();
