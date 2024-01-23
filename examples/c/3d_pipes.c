ffb6f0532b30e92eb4f65a156779227a6c9038dd
s_mack <48527177+s-mackeo@users.noreply.github.com>
2024-01-23 14:44:07 +0100

    Update 3d_pipes.c contributor name
    Signed-off-by: s_mack <48527177+s-mackeo@users.noreply.github.com>

--- examples/c/3d_pipes.c
+++ examples/c/3d_pipes.c
@@ -1,7 +1,7 @@
 /*
 MIT License
 
-Copyright (c) 2024 Jaysmito Mukherjee (jaysmito101@gmail.com)
+Copyright (c) 2024 s-mackeo (https://github.com/s-mackeo)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
