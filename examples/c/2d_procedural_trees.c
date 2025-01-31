031e1c709f15c704e489e4d73d1f3d371845231b
Jaysmito Mukherjee <73700725+Jaysmito101@users.noreply.github.com>
2025-01-31 22:38:06 +0530

    feat: update 2d_procedural_trees.c for wasm support
    Signed-off-by: Jaysmito Mukherjee <73700725+Jaysmito101@users.noreply.github.com>

--- examples/c/2d_procedural_trees.c
+++ examples/c/2d_procedural_trees.c
@@ -23,10 +23,22 @@ SOFTWARE.
 */
 
 
-#define CGL_EXCLUDE_NETWORKING
 #define CGL_LOGGING_ENABLED
+#define CGL_EXCLUDE_AUDIO
+#define CGL_EXCLUDE_NETWORKING
+#define CGL_EXCLUDE_TEXT_RENDER
 #define CGL_IMPLEMENTATION
 #include "cgl.h"
+
+
+
+#ifdef CGL_WASM
+#include <emscripten/emscripten.h>
+#include <emscripten/html5.h>
+#else 
+#define EM_BOOL int
+#endif
+
 
 #define MAX_BRANCHES 100000
 #define MAX_GENERATE_TREE_RECURSIVE_CALLS 1000000
@@ -164,19 +176,19 @@ static void generate_tree()
     generate_tree_recursive(0);
 }
 
-int main()
-{
+CGL_float prev_time = 0.0, curr_time = 0.0f, delta_time = 0.0f, frame_time = 0.0f;
+CGL_int frame_count = 0, frame_rate = 60;
+
+CGL_bool init() {
     srand((uint32_t)time(NULL));
-    if(!CGL_init()) return EXIT_FAILURE; // initialize CGL (required for setting up internals of CGL)
+    if(!CGL_init()) return CGL_FALSE; // initialize CGL (required for setting up internals of CGL)
     g_context.window_height = g_context.window_width = 700;
     g_context.window = CGL_window_create(g_context.window_width, g_context.window_height, "Procedural Trees - Jaysmito Mukherjee"); // create the window
     CGL_window_make_context_current(g_context.window); // make the opengl context for the window current
-    if(!CGL_gl_init()) return EXIT_FAILURE; // initialize cgl opengl module    
+    if(!CGL_gl_init()) return CGL_FALSE; // initialize cgl opengl module    
     g_context.framebuffer = CGL_framebuffer_create_from_default(g_context.window); // load the default framebuffer (0) into CGL_framebuffer object
-    if(!CGL_widgets_init()) return EXIT_FAILURE; 
+    if(!CGL_widgets_init()) return CGL_FALSE; 
 
-    CGL_float prev_time = CGL_utils_get_time(), curr_time = 0.0f, delta_time = 0.0f, frame_time = 0.0f;
-    CGL_int frame_count = 0, frame_rate = 60;
 
     g_tree.angle_left = CGL_deg_to_rad(30.0f);
     g_tree.angle_right = CGL_deg_to_rad(30.0f);
@@ -186,62 +198,92 @@ int main()
     g_tree.min_branch_thickness = 0.0001f;
     g_tree.min_branch_length = 0.01f;
 
-    // the main loop
-    while(!CGL_window_should_close(g_context.window)) // run till the close button is clicked
-    {
-        curr_time = CGL_utils_get_time();
-        delta_time = curr_time - prev_time; prev_time = curr_time; frame_time += delta_time; frame_count++;
-        if(frame_time >= 1.0f) { frame_rate = frame_count; frame_time = 0.0f; frame_count = 0; }
+    prev_time = CGL_utils_get_time();
 
-        CGL_window_set_size(g_context.window, g_context.window_width, g_context.window_height); // set the window size
-        // rendering
-        CGL_framebuffer_bind(g_context.framebuffer); // bind default framebuffer and also adjust viewport size and offset
-        CGL_gl_clear(0.2f, 0.2f, 0.2f, 1.0f); // clear screen with a dark gray color
+    return CGL_TRUE;
+}
 
-        generate_tree();
-
-        CGL_widgets_begin();
-        draw_tree();
-        CGL_widgets_set_fill_colorf(1.0f, 1.0f, 1.0f, 1.0f);
-        static CGL_byte text_buffer[256];
-        sprintf(text_buffer, "Delta Time: %f", delta_time);
-        CGL_widgets_add_string(text_buffer, -1.0f, 0.95f, 0.8f, 0.05f);
-        sprintf(text_buffer, "Frame Rate: %d", frame_rate);
-        CGL_widgets_add_string(text_buffer, 0.0f, 0.95f, 0.8f, 0.05f);
-        sprintf(text_buffer, "Branch Count: %d", g_tree.branch_count);
-        CGL_widgets_add_string(text_buffer, -1.0f, 0.90f, 0.8f, 0.05f);
-        sprintf(text_buffer, "Angles: (%d, %d)", (CGL_int)CGL_rad_to_deg(g_tree.angle_left), (CGL_int)CGL_rad_to_deg(g_tree.angle_right));
-        CGL_widgets_add_string(text_buffer, 0.0f, 0.90f, 0.8f, 0.05f);
-        sprintf(text_buffer, "Length Factor: %f", g_tree.length_factor);
-        CGL_widgets_add_string(text_buffer, -1.0f, 0.85f, 0.8f, 0.05f);
-        sprintf(text_buffer, "Thickness Factor: %f", g_tree.thickness_factor);
-        CGL_widgets_add_string(text_buffer, 0.0f, 0.85f, 0.8f, 0.05f);
-        sprintf(text_buffer, "Branch Chance: %f", g_tree.branch_chance);
-        CGL_widgets_add_string(text_buffer, -1.0f, 0.80f, 0.8f, 0.05f);
-        sprintf(text_buffer, "Recursive Calls: %d", generate_tree_recursive_calls);
-        CGL_widgets_add_string(text_buffer, 0.0f, 0.80f, 0.8f, 0.05f);
-        CGL_widgets_end();
-        
-
-        CGL_window_swap_buffers(g_context.window); // swap framebuffers
-        CGL_window_poll_events(g_context.window); // poll events (if this is not called every frame window will stop responding)
-
-        ADD_INPUT(g_tree.angle_left, CGL_KEY_A, 0.005f, 0.0f, CGL_PI);
-        ADD_INPUT(g_tree.angle_right, CGL_KEY_D, 0.005f, 0.0f, CGL_PI);
-        ADD_INPUT(g_tree.length_factor, CGL_KEY_W, 0.001f, 0.0f, 0.8f);
-        ADD_INPUT(g_tree.thickness_factor, CGL_KEY_S, 0.001f, 0.0f, 1.0f);
-        ADD_INPUT(g_tree.branch_chance, CGL_KEY_Q, 0.001f, 0.0f, 1.0f);
-
-        if(CGL_window_get_key(g_context.window, CGL_KEY_R) == CGL_PRESS) g_tree.seed = (CGL_int)(CGL_utils_get_time() * 10000.0f);
-
-        if(CGL_window_get_key(g_context.window, CGL_KEY_ESCAPE) == CGL_PRESS) break; // quit on pressing escape
-    }
-
-    // cleanup
+void cleanup() {
     CGL_widgets_shutdown();
     CGL_framebuffer_destroy(g_context.framebuffer); // destory framebuffer object
     CGL_gl_shutdown(); // shutdown cgl opengl module
     CGL_window_destroy(g_context.window); // destroy window
     CGL_shutdown(); // shutdown cgl and clean up resources allocated by CGL internally (if any)
+}
+
+EM_BOOL loop(double time, void* userData) {
+    (void)time;
+    (void)userData;
+
+    curr_time = CGL_utils_get_time();
+    delta_time = curr_time - prev_time; prev_time = curr_time; frame_time += delta_time; frame_count++;
+    if(frame_time >= 1.0f) { frame_rate = frame_count; frame_time = 0.0f; frame_count = 0; }
+    
+    CGL_window_set_size(g_context.window, g_context.window_width, g_context.window_height); // set the window size
+    
+    // rendering
+    CGL_framebuffer_bind(g_context.framebuffer); // bind default framebuffer and also adjust viewport size and offset
+    CGL_gl_clear(0.2f, 0.2f, 0.2f, 1.0f); // clear screen with a dark gray color
+    generate_tree();
+    CGL_widgets_begin();
+    draw_tree();
+    CGL_widgets_set_fill_colorf(1.0f, 1.0f, 1.0f, 1.0f);
+    static CGL_byte text_buffer[256];
+    sprintf(text_buffer, "Delta Time: %f", delta_time);
+    CGL_widgets_add_string(text_buffer, -1.0f, 0.95f, 0.8f, 0.05f);
+    sprintf(text_buffer, "Frame Rate: %d", frame_rate);
+    CGL_widgets_add_string(text_buffer, 0.0f, 0.95f, 0.8f, 0.05f);
+    sprintf(text_buffer, "Branch Count: %d", g_tree.branch_count);
+    CGL_widgets_add_string(text_buffer, -1.0f, 0.90f, 0.8f, 0.05f);
+    sprintf(text_buffer, "Angles: (%d, %d)", (CGL_int)CGL_rad_to_deg(g_tree.angle_left), (CGL_int)CGL_rad_to_deg(g_tree.angle_right));
+    CGL_widgets_add_string(text_buffer, 0.0f, 0.90f, 0.8f, 0.05f);
+    sprintf(text_buffer, "Length Factor: %f", g_tree.length_factor);
+    CGL_widgets_add_string(text_buffer, -1.0f, 0.85f, 0.8f, 0.05f);
+    sprintf(text_buffer, "Thickness Factor: %f", g_tree.thickness_factor);
+    CGL_widgets_add_string(text_buffer, 0.0f, 0.85f, 0.8f, 0.05f);
+    sprintf(text_buffer, "Branch Chance: %f", g_tree.branch_chance);
+    CGL_widgets_add_string(text_buffer, -1.0f, 0.80f, 0.8f, 0.05f);
+    sprintf(text_buffer, "Recursive Calls: %d", generate_tree_recursive_calls);
+    CGL_widgets_add_string(text_buffer, 0.0f, 0.80f, 0.8f, 0.05f);
+
+    // tell the controls
+    CGL_widgets_add_string("W or S: Length Factor", -1.0f, -0.95f, 0.8f, 0.05f);
+    CGL_widgets_add_string("A or D: Angle", -1.0f, -0.90f, 0.8f, 0.05f);
+    CGL_widgets_add_string("Q: Branch Chance", -1.0f, -0.85f, 0.8f, 0.05f);
+    CGL_widgets_add_string("R: Randomize Seed", -1.0f, -0.80f, 0.8f, 0.05f);
+    CGL_widgets_add_string("Press any key below and Up or Down:", -1.0f, -0.75f, 1.0f, 0.05f);
+
+
+    CGL_widgets_end();
+    
+    CGL_window_swap_buffers(g_context.window); // swap framebuffers
+    CGL_window_poll_events(g_context.window); // poll events (if this is not called every frame window will stop responding)
+    
+    ADD_INPUT(g_tree.angle_left, CGL_KEY_A, 0.005f, 0.0f, CGL_PI);
+    ADD_INPUT(g_tree.angle_right, CGL_KEY_D, 0.005f, 0.0f, CGL_PI);
+    ADD_INPUT(g_tree.length_factor, CGL_KEY_W, 0.001f, 0.0f, 0.8f);
+    ADD_INPUT(g_tree.thickness_factor, CGL_KEY_S, 0.001f, 0.0f, 1.0f);
+    ADD_INPUT(g_tree.branch_chance, CGL_KEY_Q, 0.001f, 0.0f, 1.0f);
+
+
+    
+    if(CGL_window_get_key(g_context.window, CGL_KEY_R) == CGL_PRESS) g_tree.seed = (CGL_int)(CGL_utils_get_time() * 10000.0f);
+    if(CGL_window_get_key(g_context.window, CGL_KEY_ESCAPE) == CGL_PRESS) return CGL_FALSE; // quit on pressing escape key
+    
+    return CGL_TRUE;
+}
+
+int main()
+{
+if(!init()) return EXIT_FAILURE; // initialize the application
+
+#ifdef CGL_WASM
+    emscripten_request_animation_frame_loop(loop, NULL); // start the main loop
+#else
+    while(!CGL_window_should_close(g_context.window)) {
+        if(!loop(0.0, NULL)) break; // run the loop and break if loop returns false
+    }
+    cleanup();
+#endif
     return EXIT_SUCCESS;
 }
