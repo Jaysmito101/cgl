b6ef0cc36787fcbdc11e45d2ed68f87ef183926a
Jaysmito Mukherjee <73700725+Jaysmito101@users.noreply.github.com>
2025-02-02 03:39:21 +0530

    feat: update simple_quad_tree.c for wasm support
    Signed-off-by: Jaysmito Mukherjee <73700725+Jaysmito101@users.noreply.github.com>

--- examples/c/simple_quad_tree.c
+++ examples/c/simple_quad_tree.c
@@ -21,8 +21,16 @@ SOFTWARE.
 #define CGL_EXCLUDE_NETWORKING
 #define CGL_LOGGING_ENABLED
 #define CGL_EXCLUDE_AUDIO
+#define CGL_EXCLUDE_TEXT_RENDER
 #define CGL_IMPLEMENTATION
 #include "cgl.h"
+
+#ifdef CGL_WASM
+#include <emscripten/emscripten.h>
+#include <emscripten/html5.h>
+#else
+#define EM_BOOL int
+#endif
 
 #define MAX_POINTS 1000
 #define POINT_SIZE 0.01f
@@ -56,6 +64,8 @@ CGL_void init_points()
 
 CGL_void update_points(CGL_float dt, CGL_float tm)
 {
+    (void)dt;
+
     for (CGL_sizei i = 0; i < MAX_POINTS; i++)
     {
 		points[i].y = points_clone[i].y + sinf(points[i].y - points_clone[i].y + tm + i) * 0.1f;
@@ -76,7 +86,7 @@ CGL_void render_quad_tree()
 {
     // NOTE: this is only for debugging purposes and
     //       the cgl_nd_tree data ideally should not be accessed directly
-    for (CGL_sizei i = 0; i < quad_tree->nodes_bank_size; i++)
+    for (CGL_sizei i = 0; i < (CGL_sizei)quad_tree->nodes_bank_size; i++)
     {
         if (quad_tree->aabb_intersects_aabb_function(2, quad_tree->nodes_bank[i].aabb_min, quad_tree->nodes_bank[i].aabb_max, selector_aabb_min, selector_aabb_max )) CGL_widgets_set_stroke_colorf(0.4f, 0.4f, 0.6f, 0.6f);
         else CGL_widgets_set_stroke_colorf(0.4f, 0.4f, 0.4f, 0.6f);
@@ -89,10 +99,9 @@ CGL_void render_quad_tree()
     }
 }
 
-
-CGL_int main()
+CGL_bool init()
 {
-	CGL_init();
+    CGL_init();
     mouse_selector_size = CGL_vec2_init(0.369f, 0.214f);
     g_context.window_height = g_context.window_width = 700;
     g_context.window = CGL_window_create(g_context.window_width, g_context.window_height, "Quad Tree - Jaysmito Mukherjee"); // create the window
@@ -107,86 +116,98 @@ CGL_int main()
 
     quad_tree = CGL_quad_tree_create(sizeof(CGL_sizei), 128, 100000, 100000, true);
 
-    CGL_int frame_rate = 0, frames = 0;
-    CGL_float timer = 0.0f, time_last = CGL_utils_get_time(), time_now = 0.0f;
+    return CGL_TRUE;
+}
 
-    // the main loop
-    while (!CGL_window_should_close(g_context.window)) // run till the close button is clicked
-    {
-        CGL_double mdx, mdy; CGL_window_get_mouse_position(g_context.window, &mdx, &mdy);
-        CGL_float mx = (CGL_float)mdx / (CGL_float)g_context.window_width * 2.0f - 1.0f;
-        CGL_float my = (1.0f - (CGL_float)mdy / (CGL_float)g_context.window_height) * 2.0f - 1.0f;
-        time_now = CGL_utils_get_time(); timer += time_now - time_last; time_last = time_now; frames++;
-        if (timer > 1.0f) { frame_rate = frames; frames = 0; timer = 0.0f; }
-
-
-        CGL_window_set_size(g_context.window, g_context.window_width, g_context.window_height); // set window size
-        CGL_framebuffer_bind(g_context.framebuffer); // bind default framebuffer and also adjust viewport size and offset
-        CGL_gl_clear(0.2f, 0.2f, 0.2f, 1.0f); // clear screen with a dark gray color
-
-        if(mode == 0) CGL_quad_tree_reset(quad_tree, -1.0f, -1.0f, 1.0f, 1.0f, 12, 6, CGL_window_is_key_pressed(g_context.window, CGL_KEY_F));
-
-        update_points(0.001f, CGL_utils_get_time());
-
-        CGL_widgets_begin();
-
-        CGL_widgets_set_fill_mode(true);
-        CGL_widgets_set_fill_colorf(0.6f, 0.6f, 0.6f, 1.0f);
-        render_points();
-
-        // selected_points_count = CGL_quad_tree_get_items_in_range(quad_tree, mx - mouse_selector_size.x / 2.0f, my - mouse_selector_size.y / 2.0f, mouse_selector_size.x, mouse_selector_size.y, selected_points, MAX_POINTS);
-        selector_aabb_min[0] = mx - mouse_selector_size.x / 2.0f; selector_aabb_min[1] = my - mouse_selector_size.y / 2.0f;
-        selector_aabb_max[0] = mx + mouse_selector_size.x / 2.0f; selector_aabb_max[1] = my + mouse_selector_size.y / 2.0f;
-        if (mode == 0) selected_points_count = CGL_nd_tree_get_items_in_range(quad_tree, selector_aabb_min, selector_aabb_max, selected_points, MAX_POINTS);
-
-        CGL_widgets_set_fill_colorf(0.0f, 0.8f, 0.0f, 1.0f);
-        if (mode == 0) { for (CGL_int i = 0; i < selected_points_count; i++) CGL_widgets_add_rect2f(points[selected_points[i]].x, points[selected_points[i]].y, POINT_SIZE, POINT_SIZE); }
-        else if(mode == 1)
-        {
-            for (CGL_int i = 0; i < MAX_POINTS; i++) 
-                if (CGL_aabb_contains_point_2d(2, selector_aabb_min, selector_aabb_max, &points[i]))
-                    CGL_widgets_add_rect2f(points[i].x, points[i].y, POINT_SIZE, POINT_SIZE);
-        }
-
-               
-
-
-        CGL_widgets_set_fill_mode(false);
-        CGL_widgets_set_stroke_thickness(0.005f);
-        CGL_widgets_set_stroke_colorf(0.0f, 0.8f, 0.0f, 1.0f);
-        CGL_widgets_add_rect2f(mx - mouse_selector_size.x / 2.0f, my - mouse_selector_size.y / 2.0f, mouse_selector_size.x, mouse_selector_size.y);
-
-        CGL_widgets_set_stroke_thickness(0.005f);
-        if(mode == 0 && CGL_window_is_key_pressed(g_context.window, CGL_KEY_Q)) render_quad_tree();
-
-
-        static CGL_byte frame_rate_string[32];
-        sprintf(frame_rate_string, "FPS: %d", frame_rate);
-        CGL_widgets_add_string(frame_rate_string, -0.99f, 0.90f, 0.2f, 0.05f);
-        CGL_widgets_add_string(mode == 0 ? "QT" : "NM", -0.99f, 0.80f, 0.1f, 0.05f);
-
-
-        CGL_widgets_end();
-
-        if (CGL_window_is_key_pressed(g_context.window, CGL_KEY_SPACE))
-        {
-            mode = (mode + 1) % 2;
-            CGL_utils_sleep(100);
-        }
-
-        CGL_window_swap_buffers(g_context.window); // swap framebuffers
-        CGL_window_poll_events(g_context.window); // poll events (if this is not called every frame window will stop responding)
-
-        if (CGL_window_get_key(g_context.window, CGL_KEY_ESCAPE) == CGL_PRESS) break; // quit on pressing escape
-    }
-
-    
-    // cleanup
+void cleanup()
+{
     CGL_nd_tree_destroy(quad_tree);
     CGL_widgets_shutdown();
     CGL_framebuffer_destroy(g_context.framebuffer); // destory framebuffer object
     CGL_gl_shutdown(); // shutdown cgl opengl module
     CGL_window_destroy(g_context.window); // destroy window
-	CGL_shutdown();
-	return EXIT_SUCCESS;
+    CGL_shutdown();
+}
+
+EM_BOOL loop(double time, void* userData)
+{
+    (void)time;
+    (void)userData;
+
+    static CGL_int frame_rate = 0, frames = 0;
+    static CGL_float timer = 0.0f, time_last = 0.0, time_now = 0.0f;
+
+    CGL_double mdx, mdy; CGL_window_get_mouse_position(g_context.window, &mdx, &mdy);
+    CGL_float mx = (CGL_float)mdx / (CGL_float)g_context.window_width * 2.0f - 1.0f;
+    CGL_float my = (1.0f - (CGL_float)mdy / (CGL_float)g_context.window_height) * 2.0f - 1.0f;
+    time_now = CGL_utils_get_time(); timer += time_now - time_last; time_last = time_now; frames++;
+    if (timer > 1.0f) { frame_rate = frames; frames = 0; timer = 0.0f; }
+
+    CGL_window_set_size(g_context.window, g_context.window_width, g_context.window_height); // set window size
+    CGL_framebuffer_bind(g_context.framebuffer); // bind default framebuffer and also adjust viewport size and offset
+    CGL_gl_clear(0.2f, 0.2f, 0.2f, 1.0f); // clear screen with a dark gray color
+
+    if(mode == 0) CGL_quad_tree_reset(quad_tree, -1.0f, -1.0f, 1.0f, 1.0f, 12, 6, CGL_window_is_key_pressed(g_context.window, CGL_KEY_F));
+
+    update_points(0.001f, CGL_utils_get_time());
+
+    CGL_widgets_begin();
+
+    CGL_widgets_set_fill_mode(true);
+    CGL_widgets_set_fill_colorf(0.6f, 0.6f, 0.6f, 1.0f);
+    render_points();
+
+    // selected_points_count = CGL_quad_tree_get_items_in_range(quad_tree, mx - mouse_selector_size.x / 2.0f, my - mouse_selector_size.y / 2.0f, mouse_selector_size.x, mouse_selector_size.y, selected_points, MAX_POINTS);
+    selector_aabb_min[0] = mx - mouse_selector_size.x / 2.0f; selector_aabb_min[1] = my - mouse_selector_size.y / 2.0f;
+    selector_aabb_max[0] = mx + mouse_selector_size.x / 2.0f; selector_aabb_max[1] = my + mouse_selector_size.y / 2.0f;
+    if (mode == 0) selected_points_count = CGL_nd_tree_get_items_in_range(quad_tree, selector_aabb_min, selector_aabb_max, selected_points, MAX_POINTS);
+
+    CGL_widgets_set_fill_colorf(0.0f, 0.8f, 0.0f, 1.0f);
+    if (mode == 0) { for (CGL_sizei i = 0; i < selected_points_count; i++) CGL_widgets_add_rect2f(points[selected_points[i]].x, points[selected_points[i]].y, POINT_SIZE, POINT_SIZE); }
+    else if(mode == 1)
+    {
+        for (CGL_int i = 0; i < MAX_POINTS; i++) 
+            if (CGL_aabb_contains_point_2d(2, selector_aabb_min, selector_aabb_max, (CGL_float*)&points[i]))
+                CGL_widgets_add_rect2f(points[i].x, points[i].y, POINT_SIZE, POINT_SIZE);
+    }
+
+    CGL_widgets_set_fill_mode(false);
+    CGL_widgets_set_stroke_thickness(0.005f);
+    CGL_widgets_set_stroke_colorf(0.0f, 0.8f, 0.0f, 1.0f);
+    CGL_widgets_add_rect2f(mx - mouse_selector_size.x / 2.0f, my - mouse_selector_size.y / 2.0f, mouse_selector_size.x, mouse_selector_size.y);
+
+    CGL_widgets_set_stroke_thickness(0.005f);
+    if(mode == 0 && CGL_window_is_key_pressed(g_context.window, CGL_KEY_Q)) render_quad_tree();
+
+    static CGL_byte frame_rate_string[32];
+    sprintf(frame_rate_string, "FPS: %d", frame_rate);
+    CGL_widgets_add_string(frame_rate_string, -0.99f, 0.90f, 0.2f, 0.05f);
+    CGL_widgets_add_string(mode == 0 ? "QT" : "NM", -0.99f, 0.80f, 0.1f, 0.05f);
+
+    CGL_widgets_end();
+
+    if (CGL_window_is_key_pressed(g_context.window, CGL_KEY_SPACE))
+    {
+        mode = (mode + 1) % 2;
+        CGL_utils_sleep(100);
+    }
+
+    CGL_window_swap_buffers(g_context.window); // swap framebuffers
+    CGL_window_poll_events(g_context.window); // poll events (if this is not called every frame window will stop responding)
+
+    if (CGL_window_get_key(g_context.window, CGL_KEY_ESCAPE) == CGL_PRESS) return CGL_FALSE; // quit on pressing escape
+
+    return !CGL_window_should_close(g_context.window);
+}
+
+int main()
+{
+    if(!init()) return 1;
+#ifdef CGL_WASM
+    emscripten_request_animation_frame_loop(loop, NULL);
+#else
+    while(!CGL_window_should_close(g_context.window)) loop(0, NULL);
+    cleanup();
+#endif
+    return 0;
 }
