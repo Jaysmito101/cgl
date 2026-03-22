4d9fa9c54fdf190185562e217364e00b6042e072
Jaysmito101 <jaysmito101@gmail.com>
2026-03-22 23:36:03 +0530

    feat: remove networking form example that dont need it

--- examples/c/generic_mxn_matrix.c
+++ examples/c/generic_mxn_matrix.c
@@ -25,53 +25,51 @@ SOFTWARE.
 #define CGL_LOGGING_ENABLED
 #define CGL_IMPLEMENTATION
 #define CGL_EXCLUDE_TEXT_RENDER
+#define CGL_EXCLUDE_NETWORKING
 #include "cgl.h"
 
+int main() {
+  CGL_init();
 
+  CGL_matrix *mat = CGL_matrix_identity(4, 3);
+  CGL_info("Original Matrix");
+  CGL_matrix_print(mat);
 
-int main()
-{
-    CGL_init();
+  CGL_matrix *mat2 = CGL_matrix_submatrix(mat, 0, 0, 3, 3);
+  CGL_info("Submatrix");
+  CGL_matrix_print(mat2);
 
-    CGL_matrix* mat = CGL_matrix_identity(4, 3);
-    CGL_info("Original Matrix");
-    CGL_matrix_print(mat);
+  CGL_matrix_set_elem(mat2, 0, 0, 6.0f);
+  CGL_info("Submatrix after setting element");
+  CGL_matrix_print(mat2);
 
-    CGL_matrix* mat2 = CGL_matrix_submatrix(mat, 0, 0, 3, 3);
-    CGL_info("Submatrix");
-    CGL_matrix_print(mat2);
+  CGL_matrix *mat5 = CGL_matrix_copy(mat2);
+  CGL_info("Copy of submatrix");
+  CGL_matrix_print(mat5);
 
-    CGL_matrix_set_elem(mat2, 0, 0, 6.0f);
-    CGL_info("Submatrix after setting element");
-    CGL_matrix_print(mat2);
+  CGL_matrix *mat6 = CGL_matrix_transpose(mat2);
+  CGL_info("Transpose of submatrix");
+  CGL_matrix_print(mat6);
 
-    CGL_matrix* mat5 = CGL_matrix_copy(mat2);
-    CGL_info("Copy of submatrix");
-    CGL_matrix_print(mat5);
+  CGL_matrix *mat3 = CGL_matrix_mul(mat5, mat6);
+  CGL_info("Matrix multiplication");
+  CGL_matrix_print(mat3);
 
-    CGL_matrix* mat6 = CGL_matrix_transpose(mat2);
-    CGL_info("Transpose of submatrix");
-    CGL_matrix_print(mat6);    
+  CGL_matrix *mat4 = CGL_matrix_inverse(mat2);
+  CGL_info("Inverse of submatrix");
+  CGL_matrix_print(mat4);
 
-    CGL_matrix* mat3 = CGL_matrix_mul(mat5, mat6);
-    CGL_info("Matrix multiplication");
-    CGL_matrix_print(mat3);
+  CGL_matrix *mat7 = CGL_matrix_mul(mat2, mat4);
+  CGL_info("Matrix multiplication");
+  CGL_matrix_print(mat7);
 
-    CGL_matrix* mat4 = CGL_matrix_inverse(mat2);
-    CGL_info("Inverse of submatrix");
-    CGL_matrix_print(mat4);
-
-    CGL_matrix* mat7 = CGL_matrix_mul(mat2, mat4);
-    CGL_info("Matrix multiplication");
-    CGL_matrix_print(mat7);
-
-    CGL_matrix_destroy(mat);
-    CGL_matrix_destroy(mat2);
-    CGL_matrix_destroy(mat3);
-    CGL_matrix_destroy(mat4);
-    CGL_matrix_destroy(mat5);
-    CGL_matrix_destroy(mat6);
-    CGL_matrix_destroy(mat7);
-    CGL_shutdown();
-    return 0;
+  CGL_matrix_destroy(mat);
+  CGL_matrix_destroy(mat2);
+  CGL_matrix_destroy(mat3);
+  CGL_matrix_destroy(mat4);
+  CGL_matrix_destroy(mat5);
+  CGL_matrix_destroy(mat6);
+  CGL_matrix_destroy(mat7);
+  CGL_shutdown();
+  return 0;
 }
